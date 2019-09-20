29d5f183ff0ff321433631902d3bd929d2aff639
Benjamin <468Benjamin468@gmail.com>
2019-09-20 14:46:53 -0400

    finished the clients page, seperated the header to use for other pages

---
+++ OtherPages/MainHeader.js
@@ -0,0 +1,23 @@
+document.write('<!--top navigation and title-->');
+document.write('<header>');
+document.write('	<div class = "Head">');
+document.write('		<h1>Customers</h1>');
+
+document.write('		<nav>');
+document.write('			<a href="#" class="HeadButton Accounts">');
+document.write('				<img />');
+document.write('				<h2>Accounts</h2>');
+document.write('			</a>');
+
+document.write('			<a href="#" class="HeadButton Clients">');
+document.write('				<img />');
+document.write('				<h2>Clients</h2>');
+document.write('			</a>');
+	
+document.write('			<a href="#" class="HeadButton SignOut">');
+document.write('				<img />');
+document.write('				<h2>Sign Out</h2>');
+document.write('			</a>');
+document.write('		</nav>');
+document.write('	</div>');
+document.write('</header> ');
