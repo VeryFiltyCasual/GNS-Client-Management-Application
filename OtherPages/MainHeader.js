95373986c1b1dfd05d31053a9764bf261b2d4578
Ben Matos <468Benjamin468@gmail.com>
2019-11-07 17:15:12 -0500

    first backup in a while. Added all the pages in a seperate folder. finished the html and styles of most pages (not the form page for the client). Adding jquery for style transitions

--- OtherPages/MainHeader.js
+++ OtherPages/MainHeader.js
@@ -4,22 +4,22 @@ function display(name){
 	document.write('	<div class="head"> ');
 	document.write('		<h1>' + name + '</h1>');
 
-	document.write('		<nav>');
-	document.write('			<a href="#" class="HeadButton Accounts">');
-	document.write('				<img />');
+	document.write('		<nav><ul>');
+	document.write('			<li><a href="Accounts.html" class="HeadButton Accounts">');
+	document.write('				<img src="../Assets/IconAccount.png"/>');
 	document.write('				<h2>Accounts</h2>');
-	document.write('			</a>');
+	document.write('			</a></li>');
 
-	document.write('			<a href="#" class="HeadButton Clients">');
-	document.write('				<img />');
+	document.write('			<li><a href="Clients.html" class="HeadButton Clients">');
+	document.write('				<img src="../Assets/IconClients.png"/>');
 	document.write('				<h2>Clients</h2>');
-	document.write('			</a>');
+	document.write('			</a></li>');
 		
-	document.write('			<a href="#" class="HeadButton SignOut">');
-	document.write('				<img />');
+	document.write('			<li><a href="#" class="HeadButton SignOut">');
+	document.write('				<img src="../Assets/IconSignOut.png"/>');
 	document.write('				<h2>Sign Out</h2>');
-	document.write('			</a>');
-	document.write('		</nav>');
+	document.write('			</a></li>');
+	document.write('		</ul></nav>');
 	document.write('	</div>');
 	document.write('</header> ');
 }
