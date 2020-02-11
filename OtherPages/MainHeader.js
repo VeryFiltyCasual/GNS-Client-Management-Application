3a312fd0dbdf31589facf4b9b489522dcc92dc96
Ryan Wolf <ryantwolf1@gmail.com>
2020-02-11 14:47:53 -0500

    Redo header styles and delays

--- OtherPages/MainHeader.js
+++ OtherPages/MainHeader.js
@@ -5,9 +5,7 @@ function display(name){
 	document.write('		<h1>' + name + '</h1>');
 
 	document.write('		<nav><ul>');
-	document.write('			<li>');
-	document.write('				<img src="" id="profilePic" style="border-radius: 100%"/>');
-	document.write('			</li>');
+	document.write('			<img src="https://lh6.googleusercontent.com/-ivZRaAOqvv8/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3reULevCoHpXMrJfzD9WtI4xEt8qVQ/photo.jpg" id="profilePic" style="border-radius: 100%"/>');
 	document.write('			<li><a href="Accounts.html" class="HeadButton Accounts">');
 	document.write('				<i class="fas fa-user"></i>');
 	document.write('				<h2>Accounts</h2>');
